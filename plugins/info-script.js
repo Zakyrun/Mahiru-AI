a859af3e0f34fce4b62694d7ddccb24a9505d54d
oktetosupport <botzaky11@gmail.com>
2023-06-18 15:37:58 +0700

    Add files via upload

---
+++ plugins/info-script.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+
+let str = `
+🚩Sc Ini Dari:
+https://www.youtube.com/@zykobotz
+
+owner contact
+wa.me/6282289304381`
+conn. sendMessage(m. chat, {
+text: str,
+contextInfo: {
+externalAdReply: {
+title: v,
+thumbna 
