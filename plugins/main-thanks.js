a24706fa5dda717946c81705b9a6776621116a28
oktetosupport <botzaky11@gmail.com>
2023-06-18 11:49:11 +0700

    Delete plugins directory

--- plugins/main-thanks.js
+++
@@ -1,6 +0,0 @@
-let handler = async m => m.reply(`Yoi Cuyy🗿`.trim()) // Tambah sendiri kalo mau
-
-handler.customPrefix = /^thanks|Thanks|terima kasih|terimakasih$/i
-handler.command = new RegExp
-
-export default handler
