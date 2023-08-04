ec43c3bb051719b0384e70227d09350d951a3e72
siapazaky <botzaky11@gmail.com>
2023-08-04 22:43:55 +0700

    Add files via upload

--- plugins/creator.js
+++ plugins/creator.js
@@ -6,7 +6,7 @@ let nomorwa = '6282289304381'
 let nomorwa2 = '6283865775691'
 
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`6282289304381`, `${await conn.getName(nomorwa+'@s.whatsapp.net')}`, `Ky..? `, `My Skill Copas Script`, `admin@siapazaky.com`, `japanese`, `https://chat.whatsapp.com/DnQZq5BsLzsHRkCaJz8e6t`, `Developer Miharu Ai`]
+    [`6282289304381`, `${await conn.getName(nomorwa+'@s.whatsapp.net')}`, `Ky..? `, `My Skill Copas Script`, `admin@siapazaky.com`, `japanese`, `https://chat.whatsapp.com/DnQZq5BsLzsHRkCaJz8e6t`, `Developer Miharu Ai`],
     [`6283865775691`, `${await conn.getName(nomorwa2+'@s.whatsapp.net')}`, `Sarah💓 `, `Pacar Owner`, `owner@sarah.co.id`, `japanese`, `https://chat.whatsapp.com/DnQZq5BsLzsHRkCaJz8e6t`, `Pacar Owner Jangan Di Ganggu`]
   ], m)
   let caption = `👋 Hai *@${who.split("@")[0]}*, Nih Owner *${conn.user.name}* kak`
