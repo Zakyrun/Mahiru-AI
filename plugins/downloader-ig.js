399e31e02fe496c8526d969687b36d2150848e06
siapazaky <botzaky11@gmail.com>
2023-08-27 01:27:22 +0700

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,52 +1,27 @@
-/* Owner: Sazumi Viki */
-/* Asisten: Ayaka Ai */
-/* Instagram: @moe.sazumiviki */
-/* Facebook: Sazumi Viki */
-/* Github: SazumiVicky */
-/* Buy Sc Update: +6285236226786 */
-/* Source Code: https://github.com/SazumiVicky/AyakaV2 */
+import fetch from 'node-fetch';
 
+let handler = async (m, { conn, usedPrefix, args, command, text }) => {
+  if (!text) throw `LINK?`
 
-import axios from 'axios'
-
-let handler = async (m, { conn, text, command }) => {
-  if (!text) {
-    throw `*Example*: ${command} https://www.instagram.com/p/ABC123/`;
-  }
-
-  let url = encodeURIComponent(text);
-  let apiKey = `Rs-AgesuXD`;
-  let apiUrl = `https://api.itsrose.life/downloader/ig?url=${url}&apikey=${apiKey}`;
-
+  let res;
   try {
-    let startTime = new Date();
-
-	conn.sendMessage(m.chat, {
-		react: {
-			text: '🕒',
-			key: m.key,
-		}
-	})
-
-    let response = await axios.get(apiUrl);
-    let data = response.data;
-
-    if (data.status === true) {
-      let result = data.result[0];
-      let caption = `🐱 *Sukses Mengambil:* ${(new Date() - startTime) / 1000} Detik`;
-      conn.sendFile(m.chat, result.url, '', caption, m);
-    } else {
-      throw '🐱 Gagal mengunduh konten dari Instagram.';
-    }
+    res = await fetch(`https://inrl-web.onrender.com/api/insta?url=${text}`);
   } catch (error) {
-    console.error(error);
-    m.reply('Terjadi kesalahan saat mengunduh konten dari Instagram.');
-    m.reply(`Detail Error:\n\n${error.message}`);
+    throw `An error occurred: ${error.message}`;
   }
-};
 
-handler.help = ['ig', 'igdl', 'instagram'];
-handler.tags = ['downloader'];
-handler.command = /^(ig|igdl|instagram)$/i;
+  let api_response = await res.json();
+  if (!api_response || !api_response.result || api_response.result.length === 0) {
+    throw `No video found or Invalid response from API.`;
+  }
+
+  let cap = `DONE YA KAK HEHEH >,<`;
+
+  conn.sendFile(m.chat, api_response.result[0], 'instagram.mp4', cap, m);
+}
+
+handler.help = ['instagram']
+handler.tags = ['downloader']
+handler.command = /^(instagram|igdl|ig|instagramdl)$/i
 
 export default handler
